7a6ca9878348553d749378544480b410c2d74199
Micky Cook <micky@rideforthebrand.net>
2016-06-01 11:13:50 -0500

    Api template

---
+++ page-api.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ *
+ *
+ * Template Name: Repair App
+ *
+ */?>
+{
+  "username": "<?php the_field('username') ?>",
+  "status_image": "<?php the_field('repair_image') ?>",
+  "repair_status": "",
+  "repair_feed": [
+    1,
+    {
+      "feed_date": "",
+      "feed_image": "",
+      "feed_text": ""
+    }
+  ]
+}
